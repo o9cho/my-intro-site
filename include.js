16f4d6f5c1870930c3dbfde8a715ee0f373d6a47
Choi tho <jhoon0929@gmail.com>
2025-11-18 11:34:03 +0900

    Revert "bug fix"
    This reverts commit 841b04f34786a929ffa9ff59b3603c12eec60fb4.

--- include.js
+++ include.js
@@ -1,21 +1,10 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  async function loadSection(id, file) {
-    const container = document.getElementById(id);
-    if (!container) return;
+async function loadSection(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
 
-    try {
-      const res = await fetch(file);
-      const html = await res.text();
-      container.innerHTML = html;
-    } catch (err) {
-      container.innerHTML = "<p style='color:red;'>불러오기 실패</p>";
-      console.error("로드 실패:", file, err);
-    }
-  }
-
-
-  await loadSection("award", "sections/award.html");
-  await loadSection("activities", "sections/activities.html");
-  await loadSection("other", "sections/other.html");
-  await loadSection("military", "sections/military.html");
-});
+loadSection("award", "sections/award.html");
+loadSection("activities", "sections/activities.html");
+loadSection("other", "sections/other.html");
+loadSection("military", "sections/military.html");
